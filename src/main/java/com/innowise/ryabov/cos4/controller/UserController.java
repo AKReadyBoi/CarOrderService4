4c2b0a4e2429800cbc3131b155fcd71e1677d781
AKReadyBoi <akreadyboi@gmail.com>
2024-07-12 12:46:55 +0300

    initial commit

--- src/main/java/com/innowise/ryabov/cos4/controller/UserController.java
+++ src/main/java/com/innowise/ryabov/cos4/controller/UserController.java
@@ -3,6 +3,7 @@ package com.innowise.ryabov.cos4.controller;
 import com.innowise.ryabov.cos4.dto.UserDTO;
 import com.innowise.ryabov.cos4.entity.User;
 import com.innowise.ryabov.cos4.service.serviceImplementation.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,9 +23,13 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
-    @PostMapping("/saveUser/")
-    public HttpStatusCode saveUser(User user) {
-        userService.saveUser(user);
-        return HttpStatusCode.valueOf(201);
+    @PostMapping("/saveUser")
+    public ResponseEntity<Void> saveUser(@RequestBody User user) {
+        try {
+            userService.saveUser(user);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
