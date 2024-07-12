38292f6abcdb4bd23b220f4801c2072e62bf046f
AKReadyBoi <akreadyboi@gmail.com>
2024-07-12 10:24:42 +0300

    jij

---
+++ src/main/java/com/innowise/ryabov/cos4/Cos4Application.java
@@ -0,0 +1,13 @@
+package com.innowise.ryabov.cos4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cos4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cos4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/innowise/ryabov/cos4/Cos4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.ryabov.cos4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cos4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
